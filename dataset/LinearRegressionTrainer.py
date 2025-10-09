7f3f26ca3c1bb5ec2d9575feed682ae824c9708d
peiyuw2 <peiyu.wang1@student.unimelb.edu.au>
2025-10-09 13:40:39 +1100

    feat: add template code for classifiers

--- dataset/LinearRegressionTrainer.py
+++ dataset/LinearRegressionTrainer.py
@@ -1,22 +1,57 @@
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 
-from dataset.classifier_trainer import ClassifierTrainerQ3
+from classifier_trainer import ClassifierTrainerQ3
 
+import pandas as pd
+from numpy import round, clip
+
+from sklearn.linear_model import LinearRegression
+from scipy.sparse import hstack, csr_matrix
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
 class LinearRegressionTrainer(ClassifierTrainerQ3):
-    def __init__(self, train_df: pd.DataFrame, test_df: pd.DataFrame, time_feature: bool):
-        super().__init__(train_df, test_df, time_feature)
-
     def prepare_data(self):
-        pass
+        ohe = OneHotEncoder(handle_unknown='ignore')
+        ohe.fit(self.train_df[self.categorical_features])
+        train_data_ohe = ohe.transform(self.train_df[self.categorical_features])
+        test_data_ohe = ohe.transform(self.test_df[self.categorical_features])
+        train_data_numerical = csr_matrix(self.train_df[self.numerical_features].fillna(-1).values)
+        test_data_numerical = csr_matrix(self.test_df[self.numerical_features].fillna(-1).values)
+        self.X_train = hstack([train_data_ohe, train_data_numerical])
+        self.X_test = hstack([test_data_ohe, test_data_numerical])
+
+        le = LabelEncoder()
+        le.fit(self.train_df['IncidentGrade'])
+        self.y_train = le.transform(self.train_df['IncidentGrade'])
+        self.y_test = le.transform(self.test_df['IncidentGrade'])
 
     def train(self):
-        pass
+        self.model = LinearRegression()
+        self.model.fit(self.X_train, self.y_train)
 
     def predict(self):
-        pass
+        self.y_predict = self.model.predict(self.X_test)
+        self.y_predict = clip(round(self.y_predict), self.y_test.min(), self.y_test.max()).astype(int)
 
     def outcome(self):
         pass
+cat_columns = ['Category', 'EntityType', 'EvidenceRole', 'SuspicionLevel', 'LastVerdict',
+               'ResourceType', 'Roles', 'AntispamDirection', 'ThreatFamily']
 
+numerical_columns = ['DeviceId', 'Sha256', 'IpAddress', 'Url', 'AccountSid', 'AccountUpn', 'AccountObjectId',
+                     'AccountName', 'DeviceName', 'NetworkMessageId', 'EmailClusterId', 'RegistryKey',
+                     'RegistryValueName', 'RegistryValueData', 'ApplicationId', 'ApplicationName',
+                     'OAuthApplicationId', 'FileName', 'FolderPath', 'ResourceIdName', 'OSFamily',
+                     'OSVersion', 'CountryCode', 'State', 'City']
 
+model = LinearRegressionTrainer(
+    train_df=pd.read_csv('train.csv', low_memory=False),
+    test_df=pd.read_csv('test.csv', low_memory=False),
+    categorical_features=cat_columns,
+    numerical_features=numerical_columns,
+    time_feature=True
+)
+model.train()
+model.predict()
+model.outcome()
--- dataset/classifier_trainer.py
+++ dataset/classifier_trainer.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 import pandas as pd
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 
 class ClassifierTrainer(ABC):
     """
@@ -14,6 +15,14 @@ class ClassifierTrainer(ABC):
     def __init__(self, train_df: pd.DataFrame, test_df: pd.DataFrame, categorical_features: list, numerical_features: list):
         self.train_df = train_df
         self.test_df = test_df
+        self.categorical_features = categorical_features
+        self.numerical_features = numerical_features
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
+        self.model = None
+        self.y_predict = None
 
     @abstractmethod
     def prepare_data(self):
@@ -27,11 +36,20 @@ class ClassifierTrainer(ABC):
     def predict(self):
         pass
 
-    @abstractmethod
     def outcome(self):
-        pass
+        accuracy = accuracy_score(self.y_test, self.y_predict)
+        recall = recall_score(self.y_test, self.y_predict, average='macro')
+        precision = precision_score(self.y_test, self.y_predict, average='macro')
+        f1 = f1_score(self.y_test, self.y_predict, average='macro')
+
+        print(f'Accuracy: {accuracy:.4f}')
+        print(f'Macro-Precision: {precision:.4f}')
+        print(f'Macro-Recall: {recall:.4f}')
+        print(f'Macro-F1 Score: {f1:.4f}')
+
 
 class ClassifierTrainerQ3(ClassifierTrainer, ABC):
-    def __init__(self, train_df: pd.DataFrame, test_df: pd.DataFrame, time_feature: bool):
-        super().__init__(train_df, test_df)
+    def __init__(self, train_df: pd.DataFrame, test_df: pd.DataFrame,
+                 categorical_features: list, numerical_features: list, time_feature: bool):
+        super().__init__(train_df, test_df, categorical_features, numerical_features)
         self.time_feature = time_feature
