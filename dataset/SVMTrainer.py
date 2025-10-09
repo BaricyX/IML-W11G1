135324f2cd07c0b9c9548a7dda16aca16f31b6c4
peiyuw2 <peiyu.wang1@student.unimelb.edu.au>
2025-10-09 20:06:15 +1100

    feat: add SVM trainer.

---
+++ dataset/SVMTrainer.py
@@ -0,0 +1,102 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
+from classifier_trainer import ClassifierTrainerQ3
+
+from numpy import round, clip
+import numpy as np
+
+
+from scipy.sparse import hstack, csr_matrix
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.svm import LinearSVC
+from sklearn.multiclass import OneVsRestClassifier
+
+class SVMTrainer(ClassifierTrainerQ3):
+
+    def prepare_data(self):
+        # if considering time feature as part of the model
+        if self.time_feature:
+            self.train_df, self.test_df = self.time_features(self.train_df, self.test_df)
+
+        ohe = OneHotEncoder(handle_unknown='ignore')
+        ohe.fit(self.train_df[self.categorical_features])
+
+        train_data_ohe = ohe.transform(self.train_df[self.categorical_features])
+        test_data_ohe = ohe.transform(self.test_df[self.categorical_features])
+
+        train_data_num = csr_matrix(self.train_df[self.numerical_features].fillna(-1).values)
+        test_data_num = csr_matrix(self.test_df[self.numerical_features].fillna(-1).values)
+
+        self.X_train = hstack([train_data_ohe, train_data_num])
+        self.X_test = hstack([test_data_ohe, test_data_num])
+
+        le = LabelEncoder()
+        le.fit(self.train_df['IncidentGrade'])
+        self.y_train = le.transform(self.train_df['IncidentGrade'])
+        self.y_test = le.transform(self.test_df['IncidentGrade'])
+
+    def train(self):
+        self.model = OneVsRestClassifier(LinearSVC(C=1, tol=1e-3, dual=False), n_jobs=-1)
+        self.model.fit(self.X_train, self.y_train)
+
+    def predict(self):
+        self.y_predict = self.model.predict(self.X_test)
+        self.y_predict = clip(round(self.y_predict), self.y_test.min(), self.y_test.max()).astype(int)
+
+    def time_features(self, train_df: pd.DataFrame, test_df: pd.DataFrame):
+        for df in [train_df, test_df]:
+            df['Timestamp'] = pd.to_datetime(df['Timestamp'], errors='coerce')
+            df['hour'] = df['Timestamp'].dt.hour
+            df['dayofweek'] = df['Timestamp'].dt.dayofweek
+            df['month'] = df['Timestamp'].dt.month
+            df['hour_sin'] = np.sin(2 * np.pi * df['hour'] / 24)
+            df['hour_cos'] = np.cos(2 * np.pi * df['hour'] / 24)
+            df['day_sin'] = np.sin(2 * np.pi * df['dayofweek'] / 7)
+            df['day_cos'] = np.cos(2 * np.pi * df['dayofweek'] / 7)
+            df['month_sin'] = np.sin(2 * np.pi * df['month'] / 12)
+            df['month_cos'] = np.cos(2 * np.pi * df['month'] / 12)
+
+        new_cols = ['hour_sin', 'hour_cos', 'day_sin', 'day_cos', 'month_sin', 'month_cos']
+        for c in new_cols:
+            if c not in self.numerical_features:
+                self.numerical_features.append(c)
+        return train_df, test_df
+
+
+cat_columns = ['Category', 'EntityType', 'EvidenceRole', 'SuspicionLevel', 'LastVerdict',
+               'ResourceType', 'Roles', 'AntispamDirection', 'ThreatFamily']
+
+numerical_columns = ['DeviceId', 'Sha256', 'IpAddress', 'Url', 'AccountSid', 'AccountUpn', 'AccountObjectId',
+                     'AccountName', 'DeviceName', 'NetworkMessageId', 'EmailClusterId', 'RegistryKey',
+                     'RegistryValueName', 'RegistryValueData', 'ApplicationId', 'ApplicationName',
+                     'OAuthApplicationId', 'FileName', 'FolderPath', 'ResourceIdName', 'OSFamily',
+                     'OSVersion', 'CountryCode', 'State', 'City']
+
+test_file = 'train.csv'
+train_file = 'test.csv'
+
+model_no_time = SVMTrainer(
+    train_df=pd.read_csv(test_file, low_memory=False, nrows=10000),
+    test_df=pd.read_csv(train_file, low_memory=False, nrows=10000),
+    categorical_features=cat_columns,
+    numerical_features=numerical_columns,
+    time_feature=False
+)
+model_no_time.prepare_data()
+model_no_time.train()
+model_no_time.predict()
+model_no_time.outcome()
+
+
+model_time = SVMTrainer(
+    train_df=pd.read_csv(train_file, low_memory=False, nrows=10000),
+    test_df=pd.read_csv(test_file, low_memory=False, nrows=10000),
+    categorical_features=cat_columns,
+    numerical_features=numerical_columns,
+    time_feature=True
+)
+model_time.prepare_data()
+model_time.train()
+model_time.predict()
+model_time.outcome()
